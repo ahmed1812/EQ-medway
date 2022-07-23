8bcc3d983b7b54c44fa5d38dfe9449517c243d50
Ahmed Fawzi <ahmedfawzi@Ahmeds-MacBook-Pro.local>
2022-07-23 00:18:21 -0500

    chang1e logo

--- js/script.js
+++ js/script.js
@@ -265,3 +265,9 @@ jQuery(function ($) {
 
 
 });
+
+const test = document.querySelector('.test p');
+test.innerHTML = test.innerText.split("").map(
+  (char, i) => 
+`<span style="transform:rotate(${i*13}deg)">${char}</span>`
+).join("");
